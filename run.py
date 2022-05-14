f2b7055a36705f0c64952802954b867cab2d2c73
Joe O'Donohoe <joeodonohoe@live.com>
2022-05-14 16:32:49 +0000

    add function structure to start game

--- run.py
+++ run.py
@@ -1,3 +1,65 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+POSSIBLE_WORDS = ['first', 'second', 'third']
+HANGMAN_DISPLAY = [
+    '-----  ',
+    '|    | ',
+    '|    O ',
+    '|   -|-', 
+    '|    /\\',
+    '|      ',
+    '-------'
+]
+
+INCORRECT_GUESSES = 0
+
+CORRECT_WORD = ''
+
+GUESS_COUNTER = 0
+
+"""
+initialize game start
+
+"""
+def initialize():
+    global CORRECT_WORD
+
+    n = random.randint(0, 2)
+    CORRECT_WORD = POSSIBLE_WORDS[n]
+
+
+print(CORRECT_WORD)
+
+"""
+start game function
+"""
+def start_game():
+    global CORRECT_WORD
+    initialize()
+    print(CORRECT_WORD)
+    while True:
+        guess = get_user_guess()
+        evaluate_guess(guess)
+
+def evaluate_guess(guess):
+    global CORRECT_WORD
+    global INCORRECT_GUESSES
+    if guess in CORRECT_WORD:
+        print('correct')
+    else:
+        print('incorrect')
+        INCORRECT_GUESSES = INCORRECT_GUESSES + 1
+    
+
+def get_user_guess():
+    global GUESS_COUNTER
+    GUESS_COUNTER = GUESS_COUNTER + 1
+    guess = input('Enter Guess #' + str(GUESS_COUNTER)+ '\n')
+    return guess
+
+def show_hangman_progress():
+    global HANGMAN_DISPLAY
+    for line in HANGMAN_DISPLAY:
+        print(line)
+show_hangman_progress()
+start_game()
